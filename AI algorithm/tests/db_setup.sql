9063b36a065a6b85d805677ccf08285c25775e2d
Alberto Pirillo <alberto.pirillo@mail.polimi.it>
2023-04-20 11:58:33 +0200

    Add stub of new DB structure

---
+++ AI algorithm/tests/db_setup.sql
@@ -0,0 +1,37 @@
+CREATE EXTENSION citext;
+CREATE DOMAIN EMAIL AS citext
+  CHECK ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
+
+CREATE TABLE worker
+(
+   worker_id SERIAL PRIMARY KEY,
+   name VARCHAR(30) NOT NULL,
+   surname VARCHAR(30) NOT NULL,
+   username VARCHAR(50) UNIQUE,
+   email EMAIL UNIQUE,
+   password VARCHAR(50) UNIQUE,
+   birth_date DATE , -- NOT NULL
+   address VARCHAR(50),
+   city VARCHAR(50),
+   country VARCHAR(50),
+   genre VARCHAR(50),
+   contact_email EMAIL,
+   telephone_number VARCHAR(50),
+   -- curriculum ??? NOT NULL,
+   embedding FLOAT4 ARRAY[512] --NOT NULL
+);
+
+CREATE TABLE job_offer
+(
+   -- company_id SERIAL REFERENCES company(company_id),
+   offer_id SERIAL PRIMARY KEY,
+   title VARCHAR(50) NOT NULL,
+   -- file ??? NOT NULL,
+   embedding FLOAT4 ARRAY[512] -- NOT NULL
+);
+
+CREATE TABLE applies_to
+(
+    offer_id SERIAL REFERENCES job_offer(offer_id),
+    worker_id SERIAL REFERENCES worker(worker_id)
+);
