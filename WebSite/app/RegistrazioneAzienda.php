8d4982fbeba603148bbceac846ba74879fc56a9d
AlbertoNoris <61833464+AlbertoNoris@users.noreply.github.com>
2023-04-28 15:55:16 +0200

    fix aienda

--- WebSite/app/RegistrazioneAzienda.php
+++ WebSite/app/RegistrazioneAzienda.php
@@ -1,4 +1,5 @@
 <?php
+    ob_start();
     if(file_exists('.env')) {
       // per il sito in locale
       $env = parse_ini_file('.env');
@@ -99,7 +100,8 @@ $dbconn = pg_connect("host=$PGHOST port=$PGPORT dbname=$PGDATABASE user=$PGUSER 
                 }
             }
         }
-    }    
+    }
+    ob_end_flush();   
     ?>
     </body>
 </html>
