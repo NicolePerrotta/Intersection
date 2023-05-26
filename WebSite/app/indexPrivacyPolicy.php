924e3efe24eda9e425e15649ea86c062ddbdcc13
Andrea Sinisi <92164128+AndreaSinisi@users.noreply.github.com>
2023-05-26 20:35:01 +0200

    Privacy Policy full text

--- WebSite/app/indexPrivacyPolicy.php
+++ WebSite/app/indexPrivacyPolicy.php
@@ -354,51 +354,127 @@ ob_start();
             <div class="col-md-8 mx-auto d-flex flex-column gap-5">
                 <h1 class="text-color-2 fw-bold">Privacy Policy</h1>
                 <div>
+                    <p>In Intersection, accessibile da https://intersection.up.railway.app, una delle nostre principali priorità è la privacy dei nostri visitatori. Questo documento sulla politica sulla privacy contiene i tipi di informazioni raccolte e registrate da Intersection e il modo in cui le utilizziamo.</p>
+
+                    <p>Se hai ulteriori domande o hai bisogno di ulteriori informazioni sulla nostra Privacy Policy, non esitare a contattarci.</p>
+                    <p>La presente Privacy Policy si applica solo alle nostre attività online ed è valida per i visitatori del nostro sito Web per quanto riguarda le informazioni che hanno condiviso e/o raccolto in Intersection. Questa politica non è applicabile alle informazioni raccolte offline o tramite canali diversi da questo sito web.</p>
+
+                    <h4>Consenso</h4>
+                    <p>Utilizzando il nostro sito Web, acconsenti alla nostra Privacy Policy e ne accetti i termini.</p>
+
+                    <h4>Informazioni che raccogliamo</h4>
+                    <p>Le informazioni personali che ti viene chiesto di fornire e i motivi per cui ti viene chiesto di fornirle ti saranno chiariti nel momento in cui ti chiediamo di fornire le tue informazioni personali.</p>
+                    <p>Se ci contatti direttamente, potremmo ricevere ulteriori informazioni su di te come nome, indirizzo e-mail, numero di telefono, il contenuto del messaggio e/o degli allegati che potresti inviarci e qualsiasi altra informazione che potresti scegliere di fornire .</p>
+                    <p>Quando ti registri per un Account, potremmo richiedere le tue informazioni di contatto, inclusi elementi come nome, ragione sociale, indirizzo, indirizzo e-mail e numero di telefono.</p>
+
+                    <h4>Come utilizziamo le tue informazioni</h4>
+                    <p>Utilizziamo le informazioni che raccogliamo in vari modi, tra cui per:</p>
+                    <ul>
+                        <li>Fornire, gestire e mantenere il nostro sito web</li>
+                        <li>Migliorare, personalizzare ed espandere il nostro sito web</li>
+                        <li>Comprendere e analizzare come utilizzi il nostro sito web</li>
+                        <li>Sviluppare nuovi prodotti, servizi, caratteristiche e funzionalità</li>
+                        <li>Comunicare con te, direttamente o tramite uno dei nostri partner, anche per il servizio clienti, per fornirti aggiornamenti e altre informazioni relative al sito Web e per scopi di marketing e promozionali</li>
+                        <li>Inviarti email</li>
+                        <li>Trovare e prevenire le frodi</li>
+                    </ul>
+
+                    <h4>File di log</h4>
+                    <p>Intersection segue una procedura standard di utilizzo dei file di registro. Questi file registrano i visitatori quando visitano i siti web. Tutte le società di hosting fanno questo e una parte dell'analisi dei servizi di hosting. Le informazioni raccolte dai file di registro includono indirizzi IP (Internet Protocol), tipo di browser, provider di servizi Internet (ISP), data e ora, pagine di riferimento/uscita ed eventualmente il numero di clic. Questi non sono collegati ad alcuna informazione di identificazione personale. Lo scopo delle informazioni è analizzare le tendenze, amministrare il sito, monitorare il movimento degli utenti sul sito Web e raccogliere informazioni demografiche.</p>
+
+                    <h4>Norme sulla privacy dei partner pubblicitari</h4>
+                    <p>Puoi consultare questo elenco per trovare l'Privacy Policy per ciascuno dei partner pubblicitari di Intersection.</p>
+                    <p>Gli ad server o le reti pubblicitarie di terze parti utilizzano tecnologie come cookie, JavaScript o Web Beacon utilizzati nei rispettivi annunci pubblicitari e collegamenti visualizzati su Intersection, che vengono inviati direttamente al browser degli utenti. Ricevono automaticamente il tuo indirizzo IP quando ciò si verifica. Queste tecnologie vengono utilizzate per misurare l'efficacia delle loro campagne pubblicitarie e/o per personalizzare i contenuti pubblicitari che vedi sui siti web che visiti.</p>
+                    <p>Tieni presente che Intersection non ha accesso o controllo su questi cookie utilizzati da inserzionisti di terze parti.</p>
+
+                    <h4>Norme sulla privacy di terze parti</h4>
+                    <p>L'Privacy Policy di Intersection non si applica ad altri inserzionisti o siti web. Pertanto, ti consigliamo di consultare le rispettive Informative sulla privacy di questi ad server di terze parti per informazioni più dettagliate. Può includere le loro pratiche e istruzioni su come rinunciare a determinate opzioni. </p>
+                    <p>Puoi scegliere di disabilitare i cookie attraverso le singole opzioni del tuo browser. Per conoscere informazioni più dettagliate sulla gestione dei cookie con browser Web specifici, è possibile trovarle sui rispettivi siti Web dei browser.</p>
+
+                    <h4>Diritti sulla privacy CCPA (Non vendere le mie informazioni personali)</h4>
+                    <p>In base al CCPA, tra gli altri diritti, i consumatori della California hanno diritto a:</p>
+                    <p>Richiedere che un'azienda che raccoglie i dati personali di un consumatore divulghi le categorie e i dati personali specifici che un'azienda ha raccolto sui consumatori.</p>
+                    <p>Richiedere che un'azienda elimini tutti i dati personali sul consumatore raccolti da un'azienda.</p>
+                    <p>Richiedere che un'azienda che vende i dati personali di un consumatore non venda i dati personali del consumatore.</p>
+                    <p>Se fai una richiesta, abbiamo un mese per risponderti. Se desideri esercitare uno di questi diritti, contattaci.</p>
+
+                    <h4>Diritti alla protezione dei dati del GDPR</h4>
+                    <p>Vorremmo assicurarci che tu sia pienamente consapevole di tutti i tuoi diritti alla protezione dei dati. Ogni utente ha diritto a quanto segue:</p>
+                    <p>Diritto di accesso: hai il diritto di richiedere copie dei tuoi dati personali. Potremmo addebitarti una piccola commissione per questo servizio.</p>
+                    <p>Il diritto alla rettifica: hai il diritto di richiedere la correzione di qualsiasi informazione che ritieni inesatta. Hai anche il diritto di richiedere che completiamo le informazioni che ritieni incomplete.</p>
+                    <p>Il diritto alla cancellazione: hai il diritto di richiedere la cancellazione dei tuoi dati personali, a determinate condizioni.</p>
+                    <p>Il diritto di limitare il trattamento: hai il diritto di richiedere che limitiamo il trattamento dei tuoi dati personali, a determinate condizioni.</p>
+                    <p>Il diritto di opporsi al trattamento: hai il diritto di opporti al nostro trattamento dei tuoi dati personali, a determinate condizioni.</p>
+                    <p>Diritto alla portabilità dei dati: hai il diritto di richiedere il trasferimento dei dati che abbiamo raccolto a un'altra organizzazione o direttamente a te, a determinate condizioni.</p>
+                    <p>Se fai una richiesta, abbiamo un mese per risponderti. Se desideri esercitare uno di questi diritti, contattaci.</p>
+
+                    <h4>Modifiche alla presente Privacy Policy</h4>
+                    <p>Possiamo aggiornare periodicamente la nostra Privacy Policy. Pertanto, ti consigliamo di rivedere periodicamente questa pagina per eventuali modifiche. Ti informeremo di eventuali modifiche pubblicando la nuova Privacy Policy in questa pagina. Queste modifiche entrano in vigore immediatamente, dopo essere state pubblicate su questa pagina.</p>
+
+                    <h4>Contattaci</h4>
+                    <p>Se hai domande o suggerimenti sulla nostra Privacy Policy, non esitare a contattarci.</p>
+                </div>
+            </div>
+          </div>
+        </section>
+
+      </div>
+      <?php endif; ?>
+
+      <?php if( $_SESSION['lang'] == 'en' ): ?>
+      <!-- Page content EN -->
+      <div class="page-content">
+
+        <section id="main">
+          <div class="container px-3" style="padding: 60px 0 120px;">
+            <div class="col-md-8 mx-auto d-flex flex-column gap-5">
+                <h1 class="text-color-2 fw-bold">Privacy Policy</h1>
+                <div>
                     <p>At Intersection, accessible from https://intersection.up.railway.app, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Intersection and how we use it.</p>
 
                     <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
                     <p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Intersection. This policy is not applicable to any information collected offline or via channels other than this website.</p>
 
-                    <h2>Consent</h2>
+                    <h4>Consent</h4>
                     <p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
 
-                    <h2>Information we collect</h2>
+                    <h4>Information we collect</h4>
                     <p>The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.</p>
                     <p>If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.</p>
                     <p>When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.</p>
 
-                    <h2>How we use your information</h2>
+                    <h4>How we use your information</h4>
                     <p>We use the information we collect in various ways, including to:</p>
                     <ul>
-                    <li>Provide, operate, and maintain our website</li>
-                    <li>Improve, personalize, and expand our website</li>
-                    <li>Understand and analyze how you use our website</li>
-                    <li>Develop new products, services, features, and functionality</li>
-                    <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes</li>
-                    <li>Send you emails</li>
-                    <li>Find and prevent fraud</li>
+                        <li>Provide, operate, and maintain our website</li>
+                        <li>Improve, personalize, and expand our website</li>
+                        <li>Understand and analyze how you use our website</li>
+                        <li>Develop new products, services, features, and functionality</li>
+                        <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes</li>
+                        <li>Send you emails</li>
+                        <li>Find and prevent fraud</li>
                     </ul>
 
-                    <h2>Log Files</h2>
+                    <h4>Log Files</h4>
                     <p>Intersection follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
 
-                    <h2>Advertising Partners Privacy Policies</h2>
+                    <h4>Advertising Partners Privacy Policies</h4>
                     <p>You may consult this list to find the Privacy Policy for each of the advertising partners of Intersection.</p>
                     <p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Intersection, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
                     <p>Note that Intersection has no access to or control over these cookies that are used by third-party advertisers.</p>
 
-                    <h2>Third Party Privacy Policies</h2>
+                    <h4>Third Party Privacy Policies</h4>
                     <p>Intersection's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. </p>
                     <p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.</p>
 
-                    <h2>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
+                    <h4>CCPA Privacy Rights (Do Not Sell My Personal Information)</h4>
                     <p>Under the CCPA, among other rights, California consumers have the right to:</p>
                     <p>Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</p>
                     <p>Request that a business delete any personal data about the consumer that a business has collected.</p>
                     <p>Request that a business that sells a consumer's personal data, not sell the consumer's personal data.</p>
                     <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
 
-                    <h2>GDPR Data Protection Rights</h2>
+                    <h4>GDPR Data Protection Rights</h4>
                     <p>We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</p>
                     <p>The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.</p>
                     <p>The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.</p>
@@ -408,30 +484,15 @@ ob_start();
                     <p>The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.</p>
                     <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
 
-                    <h2>Children's Information</h2>
-                    <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
-                    <p>Intersection does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
-
-                    <h2>Changes to This Privacy Policy</h2>
+                    <h4>Changes to This Privacy Policy</h4>
                     <p>We may update our Privacy Policy from time to time. Thus, we advise you to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately, after they are posted on this page.</p>
 
-                    <h2>Contact Us</h2>
+                    <h4>Contact Us</h4>
                     <p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.</p>
                 </div>
             </div>
           </div>
         </section>
-
-        <script>
-          document.addEventListener('DOMContentLoaded', ()=>{
-            let workerDetails = document.querySelectorAll('.dettagli-professionista > div:not(:first-child)');
-            workerDetails.forEach((workerDetail)=>{
-              if(workerDetail.querySelector('span:not(.fw-bold)').textContent == "") {
-                workerDetail.setAttribute('style', 'display: none !important');
-              }
-            });
-          });
-        </script>
 
       </div>
       <?php endif; ?>
