1ecdd7424b42221c594d27bc1e5bbcd86171550e
Andrea Sinisi <92164128+AndreaSinisi@users.noreply.github.com>
2023-05-26 10:22:15 +0200

    indexLogin EN

--- WebSite/app/index.php
+++ WebSite/app/index.php
@@ -67,8 +67,8 @@ if( !isset( $_GET['lang'] ) ) {
                     <button class="btn dropdown-toggle fw-bold" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="border: none;">IT</button>
                     <ul class="dropdown-menu dropdown-menu-end" style="--bs-dropdown-min-width: 10px; --bs-dropdown-bg: #F9FBFE; --bs-dropdown-link-hover-color: var(--intersection-color-3); --bs-dropdown-link-active-color: var(--intersection-color-3); --bs-dropdown-link-active-bg: transparent; transition: none;">
                       <li><div class="dropdown-header">Seleziona lingua</div></li>
-                      <li><a class="dropdown-item" href="#">Italiano</a></li>
-                      <li><a class="dropdown-item" href="#">English</a></li>
+                      <li><a class="dropdown-item" href="index.php?lang=it">Italiano</a></li>
+                      <li><a class="dropdown-item" href="index.php?lang=en">English</a></li>
                     </ul>
                 </div>
                 <a href=" <?php echo 'indexListJobs.php?uid=' . $_SESSION['uid'] . '&sa=' . $_SESSION['sa'] ?> " class="btn fw-bold py-2 px-3 border border-2 rounded" style="border-color: var(--intersection-color-3) !important; color: var(--intersection-color-3) !important;">Lista lavori</a>
@@ -127,8 +127,8 @@ if( !isset( $_GET['lang'] ) ) {
                     <button class="btn dropdown-toggle fw-bold" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="border: none;">IT</button>
                     <ul class="dropdown-menu dropdown-menu-end" style="--bs-dropdown-min-width: 10px; --bs-dropdown-bg: #F9FBFE; --bs-dropdown-link-hover-color: var(--intersection-color-3); --bs-dropdown-link-active-color: var(--intersection-color-3); --bs-dropdown-link-active-bg: transparent; transition: none;">
                       <li><div class="dropdown-header">Seleziona lingua</div></li>
-                      <li><a class="dropdown-item" href="#">Italiano</a></li>
-                      <li><a class="dropdown-item" href="#">English</a></li>
+                      <li><a class="dropdown-item" href="index.php?lang=it">Italiano</a></li>
+                      <li><a class="dropdown-item" href="index.php?lang=en">English</a></li>
                     </ul>
                 </div>
                 <a href=" <?php echo 'indexJobOffers.php?uid=' . $_SESSION['uid'] . '&sa=' . $_SESSION['sa'] ?> " class="btn fw-bold py-2 px-3 border border-2 rounded" style="border-color: var(--intersection-color-3) !important; color: var(--intersection-color-3) !important;">Lista offerte</a>
--- WebSite/app/indexLogin.php
+++ WebSite/app/indexLogin.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+if( !isset( $_GET['lang'] ) ) {
+  if( !isset( $_SESSION['lang'] ) ) {
+    $_SESSION['lang'] = 'it';
+  }
+} else {
+  $_SESSION['lang'] = $_GET['lang'];
+}
 ?>
 <!DOCTYPE html>
   <html>
@@ -8,6 +15,12 @@ session_start();
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Login - Intersection</title>
+      <title>
+        <?php 
+          if( $_SESSION['lang'] == 'it' ) { echo "Login - Intersection"; }
+          if( $_SESSION['lang'] == 'en' ) { echo "Login - Intersection"; }
+        ?>
+      </title>
       <link rel="icon" href="images/favi-1.png" type="favicon">
       <link rel="stylesheet" href="css/style.css">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
@@ -19,7 +32,8 @@ session_start();
 
     <body>
 
-      <!-- Header -->
+    <?php if( $_SESSION['lang'] == 'it' ): ?>
+      <!-- Header IT -->
       <header class="top-0 w-100">
         <div class="container px-2 d-flex flex-column flex-md-row justify-content-between align-items-center gap-5 py-4">
           <div class="logo-container">
@@ -33,8 +47,8 @@ session_start();
               <button class="btn dropdown-toggle fw-bold" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="border: none;">IT</button>
                 <ul class="dropdown-menu dropdown-menu-end" style="--bs-dropdown-min-width: 10px; --bs-dropdown-bg: #F9FBFE; --bs-dropdown-link-hover-color: var(--intersection-color-3); --bs-dropdown-link-active-color: var(--intersection-color-3); --bs-dropdown-link-active-bg: transparent; transition: none;">
                   <li><div class="dropdown-header">Seleziona lingua</div></li>
-                  <li><a class="dropdown-item" href="#">Italiano</a></li>
-                  <li><a class="dropdown-item" href="#">English</a></li>
+                  <li><a class="dropdown-item" href="indexLogin.php?lang=it">Italiano</a></li>
+                  <li><a class="dropdown-item" href="indexLogin.php?lang=en">English</a></li>
                 </ul>
             </div>
             <a href="indexLogin.php" class="text-decoration-none text-color-2 fw-bold">Accedi</a>
@@ -114,8 +128,8 @@ session_start();
                     <button class="btn dropdown-toggle fw-bold" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="border: none;">IT</button>
                     <ul class="dropdown-menu dropdown-menu-end" style="--bs-dropdown-min-width: 10px; --bs-dropdown-bg: #F9FBFE; --bs-dropdown-link-hover-color: var(--intersection-color-3); --bs-dropdown-link-active-color: var(--intersection-color-3); --bs-dropdown-link-active-bg: transparent; transition: none;">
                       <li><div class="dropdown-header">Seleziona lingua</div></li>
-                      <li><a class="dropdown-item" href="#">Italiano</a></li>
-                      <li><a class="dropdown-item" href="#">English</a></li>
+                      <li><a class="dropdown-item" href="indexLogin.php?lang=it">Italiano</a></li>
+                      <li><a class="dropdown-item" href="indexLogin.php?lang=en">English</a></li>
                     </ul>
                 </div>
                 <a href=" <?php echo 'indexJobOffers.php?uid=' . $_SESSION['uid'] . '&sa=' . $_SESSION['sa'] ?> " class="btn fw-bold py-2 px-3 border border-2 rounded" style="border-color: var(--intersection-color-3) !important; color: var(--intersection-color-3) !important;">Lista offerte</a>
@@ -172,8 +186,166 @@ session_start();
           <?php endif; ?>
         </div>
       </header>
+      <?php endif; ?>
 
-      <!-- Page content -->
+      <?php if( $_SESSION['lang'] == 'en' ): ?>
+      <!-- Header EN -->
+      <header class="top-0 w-100">
+        <div class="container px-2 d-flex flex-column flex-md-row justify-content-between align-items-center gap-5 py-4">
+          <div class="logo-container">
+            <a href="index.php">
+              <img src="images/logo-1.png" alt="Intersection" style="height: 30px;">
+            </a>
+          </div>
+          <?php if( !isset( $_SESSION['uid'] ) ) : ?>
+          <div class="menu-container d-flex align-items-center gap-4">
+            <div class="dropdown">
+              <button class="btn dropdown-toggle fw-bold" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="border: none;">EN</button>
+                <ul class="dropdown-menu dropdown-menu-end" style="--bs-dropdown-min-width: 10px; --bs-dropdown-bg: #F9FBFE; --bs-dropdown-link-hover-color: var(--intersection-color-3); --bs-dropdown-link-active-color: var(--intersection-color-3); --bs-dropdown-link-active-bg: transparent; transition: none;">
+                  <li><div class="dropdown-header">Select language</div></li>
+                  <li><a class="dropdown-item" href="indexLogin.php?lang=it">Italiano</a></li>
+                  <li><a class="dropdown-item" href="indexLogin.php?lang=en">English</a></li>
+                </ul>
+            </div>
+            <a href="indexLogin.php" class="text-decoration-none text-color-2 fw-bold">Login</a>
+            <div class="dropdown">
+              <button class="btn dropdown-toggle fw-bold py-2 px-3 border border-2 rounded" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="border-color: var(--intersection-color-3) !important; color: var(--intersection-color-3)">Sign up</button>
+              <ul class="dropdown-menu dropdown-menu-end" style="--bs-dropdown-min-width: 10px; --bs-dropdown-bg: #F9FBFE; --bs-dropdown-link-hover-color: var(--intersection-color-3); --bs-dropdown-link-active-color: var(--intersection-color-3); --bs-dropdown-link-active-bg: transparent; transition: none;">
+                <li><div class="dropdown-header">Sign up as</div></li>
+                <li><a class="dropdown-item" href="indexRegistrazioneAziendale.php">Company</a></li>
+                <li><a class="dropdown-item" href="indexRegistrazione.php">Professional</a></li>
+              </ul>
+            </div>
+          </div>
+          <?php else : ?>
+            <?php if( $_SESSION['sa'] == 0 ) : ?>
+              <div class="menu-container d-flex align-items-center gap-4">
+                <div class="dropdown">
+                    <button class="btn dropdown-toggle fw-bold" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="border: none;">EN</button>
+                    <ul class="dropdown-menu dropdown-menu-end" style="--bs-dropdown-min-width: 10px; --bs-dropdown-bg: #F9FBFE; --bs-dropdown-link-hover-color: var(--intersection-color-3); --bs-dropdown-link-active-color: var(--intersection-color-3); --bs-dropdown-link-active-bg: transparent; transition: none;">
+                      <li><div class="dropdown-header">Select language</div></li>
+                      <li><a class="dropdown-item" href="#">Italiano</a></li>
+                      <li><a class="dropdown-item" href="#">English</a></li>
+                    </ul>
+                </div>
+                <a href=" <?php echo 'indexListJobs.php?uid=' . $_SESSION['uid'] . '&sa=' . $_SESSION['sa'] ?> " class="btn fw-bold py-2 px-3 border border-2 rounded" style="border-color: var(--intersection-color-3) !important; color: var(--intersection-color-3) !important;">Lista lavori</a>
+                <div class="dropdown">
+                    <button class="btn dropdown-toggle fw-bold d-flex align-items-center gap-2" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="border: none;">
+                      <?php 
+                        if( file_exists('.env') ) {
+                          // per il sito in locale
+                          $env = parse_ini_file('.env');
+                          $PGHOST = $env['PGHOST'];
+                          $PGPORT = $env['PGPORT'];
+                          $PGDATABASE = $env['PGDATABASE'];
+                          $PGUSER = $env['PGUSER'];
+                          $PGPASSWORD = $env['PGPASSWORD'];
+                        } else {
+                          // per il sito deployato
+                          $PGHOST = getenv('PGHOST');
+                          $PGPORT = getenv('PGPORT');
+                          $PGDATABASE = getenv('PGDATABASE');
+                          $PGUSER = getenv('PGUSER');
+                          $PGPASSWORD = getenv('PGPASSWORD');
+                        }
+                        $dbconn = pg_connect( "host=$PGHOST port=$PGPORT dbname=$PGDATABASE user=$PGUSER password=$PGPASSWORD" ) or header( "Location: indexErrore.php?er=100" );
+                        $q19 = "SELECT * FROM worker WHERE worker_id=$1 LIMIT 1";
+                        $result19 = pg_query_params( $dbconn, $q19, array($_SESSION['uid']) );
+                        if( pg_num_rows( $result19 ) > 0) {
+                          $co=pg_fetch_assoc($result19);  
+                          if( isset( $co['picture'] ) ) {
+                            $propic = $co['picture'];
+                            $usernameWorker = $co['username'];
+                            $profile_picture = pg_unescape_bytea($propic);
+                            $picture_filename = "storage/image_$usernameWorker.png";
+                            file_put_contents($picture_filename, $profile_picture);
+                            echo '<img src="' . $picture_filename . '" class="img-fluid rounded" style="width: 30px; aspect-ratio: 1; object-fit: cover;">';
+                            } else {
+                            echo '<img src="images/default-profile.png" class="img-fluid rounded" style="width: 30px; aspect-ratio: 1; object-fit: cover;">';
+                            }
+                        } else {
+                          echo '<img src="images/default-profile.png" class="img-fluid rounded" style="width: 30px; aspect-ratio: 1; object-fit: cover;">';
+                        }
+                        if(isset($result19)) pg_free_result($result19);
+                        pg_close($dbconn);
+                        ?>
+                      <div><?php echo $_SESSION['user'] ?></div>
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-end" style="--bs-dropdown-min-width: 10px; --bs-dropdown-bg: #F9FBFE; --bs-dropdown-link-hover-color: var(--intersection-color-3); --bs-dropdown-link-active-color: var(--intersection-color-3); --bs-dropdown-link-active-bg: transparent; transition: none;">
+                      <li><div class="dropdown-header">Actions</div></li>
+                      <li><a class="dropdown-item" href=" <?php echo 'indexUtenti.php?uid=' . $_SESSION['uid'] . '&sa=' . $_SESSION['sa'] ?> ">Profile</a></li>
+                      <li><a class="dropdown-item" href="Logout.php">Logout</a></li>
+                    </ul>
+                </div>
+              </div>
+            <?php elseif( $_SESSION['sa'] == 1 ) : ?>
+              <div class="menu-container d-flex align-items-center gap-4">
+                <div class="dropdown">
+                    <button class="btn dropdown-toggle fw-bold" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="border: none;">EN</button>
+                    <ul class="dropdown-menu dropdown-menu-end" style="--bs-dropdown-min-width: 10px; --bs-dropdown-bg: #F9FBFE; --bs-dropdown-link-hover-color: var(--intersection-color-3); --bs-dropdown-link-active-color: var(--intersection-color-3); --bs-dropdown-link-active-bg: transparent; transition: none;">
+                      <li><div class="dropdown-header">Select language</div></li>
+                      <li><a class="dropdown-item" href="#">Italiano</a></li>
+                      <li><a class="dropdown-item" href="#">English</a></li>
+                    </ul>
+                </div>
+                <a href=" <?php echo 'indexJobOffers.php?uid=' . $_SESSION['uid'] . '&sa=' . $_SESSION['sa'] ?> " class="btn fw-bold py-2 px-3 border border-2 rounded" style="border-color: var(--intersection-color-3) !important; color: var(--intersection-color-3) !important;">Lista offerte</a>
+                <div class="dropdown">
+                    <button class="btn dropdown-toggle fw-bold d-flex align-items-center gap-2" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="border: none;">
+                      <?php 
+                        if( file_exists('.env') ) {
+                          // per il sito in locale
+                          $env = parse_ini_file('.env');
+                          $PGHOST = $env['PGHOST'];
+                          $PGPORT = $env['PGPORT'];
+                          $PGDATABASE = $env['PGDATABASE'];
+                          $PGUSER = $env['PGUSER'];
+                          $PGPASSWORD = $env['PGPASSWORD'];
+                        } else {
+                          // per il sito deployato
+                          $PGHOST = getenv('PGHOST');
+                          $PGPORT = getenv('PGPORT');
+                          $PGDATABASE = getenv('PGDATABASE');
+                          $PGUSER = getenv('PGUSER');
+                          $PGPASSWORD = getenv('PGPASSWORD');
+                        }
+                        $dbconn = pg_connect( "host=$PGHOST port=$PGPORT dbname=$PGDATABASE user=$PGUSER password=$PGPASSWORD" ) or header( "Location: indexErrore.php?er=100" );
+                        $q19 = "SELECT * FROM company WHERE company_id=$1 LIMIT 1";
+                        $result19 = pg_query_params( $dbconn, $q19, array($_SESSION['uid']) );
+                        if( pg_num_rows( $result19 ) > 0) {
+                          $co=pg_fetch_assoc($result19);  
+                          if( isset( $co['logo'] ) ) {
+                            $logo = $co['logo'];
+                            $usernameCompany = $co['username'];
+                            $logo = pg_unescape_bytea($logo);
+                            $logo_filename = "storage/image_$usernameCompany.png";
+                            file_put_contents($logo_filename, $logo);
+                            echo '<img src="' . $logo_filename . '" class="img-fluid rounded" style="width: 30px; aspect-ratio: 1; object-fit: cover;">';
+                          } else {
+                            echo '<img src="images/default-profile.png" class="img-fluid rounded" style="width: 30px; aspect-ratio: 1; object-fit: cover;">';
+                          }
+                        } else {
+                          echo '<img src="images/default-profile.png" class="img-fluid rounded" style="width: 30px; aspect-ratio: 1; object-fit: cover;">';
+                        }
+                        if(isset($result19)) pg_free_result($result19);
+                        pg_close($dbconn);
+                      ?>
+                      <div><?php echo $_SESSION['user'] ?></div>
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-end" style="--bs-dropdown-min-width: 10px; --bs-dropdown-bg: #F9FBFE; --bs-dropdown-link-hover-color: var(--intersection-color-3); --bs-dropdown-link-active-color: var(--intersection-color-3); --bs-dropdown-link-active-bg: transparent; transition: none;">
+                      <li><div class="dropdown-header">Actions</div></li>
+                      <li><a class="dropdown-item" href=" <?php echo 'indexUtenti.php?uid=' . $_SESSION['uid'] . '&sa=' . $_SESSION['sa'] ?> ">Profile</a></li>
+                      <li><a class="dropdown-item" href="Logout.php">Logout</a></li>
+                    </ul>
+                </div>
+              </div>
+            <?php endif; ?>
+          <?php endif; ?>
+        </div>
+      </header>
+      <?php endif; ?>
+
+      <?php if( $_SESSION['lang'] == 'it' ): ?>
+      <!-- Page content IT -->
       <div class="page-content">
         <section id="login">
           <div class="container px-3" style="padding: 120px 0;">
@@ -200,8 +372,40 @@ session_start();
           </div>
         </section>
       </div>
+      <?php endif; ?>
 
-      <!-- Footer -->
+      <?php if( $_SESSION['lang'] == 'en' ): ?>
+      <!-- Page content EN -->
+      <div class="page-content">
+        <section id="login">
+          <div class="container px-3" style="padding: 120px 0;">
+            <div class="col-md-8 mx-auto d-flex flex-column gap-5">
+              <div>
+                <h1 class="text-color-2 fw-bold">Login</h1>
+                <div class="text-color-5">Log in with the data used during registration.</div>
+              </div>
+              <div class="d-flex flex-column gap-3">
+                <form name="formLogin" action="Login.php" method="POST">
+                  <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="text" class="form-control" name="email" id="email" required>
+                  </div>
+                  <div class="mb-4">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" name="password" id="password" required>
+                  </div>
+                  <button id="login-button" name="loginButton" type="submit" class="btn btn-primary w-100 fw-bold" style="--bs-btn-bg: var(--intersection-color-3); --bs-btn-hover-bg: var(--intersection-color-2)">Accedi</button>
+                </form>
+                <div class="text-color-5 text-center">Don't have an account yet? Sign up as <a href="indexRegistrazione.php" class="text-decoration-none text-color-2 fw-bold">professional</a> or as <a href="indexRegistrazioneAziendale.php" class="text-decoration-none text-color-2 fw-bold">company</a>.</div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <?php endif; ?>
+
+      <?php if( $_SESSION['lang'] == 'it' ): ?>
+      <!-- Footer IT -->
       <footer class="bg-color-4">
         <div class="container px-2 d-flex flex-column gap-5 py-5 text-color-5">
           <div class="footer-top d-flex flex-column flex-lg-row align-items-center gap-5">
@@ -224,6 +428,33 @@ session_start();
           </div>
         </div>
       </footer>
+      <?php endif; ?>
+
+      <?php if( $_SESSION['lang'] == 'en' ): ?>
+      <!-- Footer EN -->
+      <footer class="bg-color-4">
+        <div class="container px-2 d-flex flex-column gap-5 py-5 text-color-5">
+          <div class="footer-top d-flex flex-column flex-lg-row align-items-center gap-5">
+            <div class="w-50 d-flex flex-column flex-lg-row gap-5 align-items-center">
+              <a href="index.php">
+                <img src="images/logo-1-white.png" alt="Intersection" style="height: 30px;">
+              </a>
+              <div class="text-center text-lg-start">The first website that helps you find your next job with artificial intelligence.</div>
+            </div>
+            <div class="w-50 d-flex flex-column flex-md-row justify-content-center justify-content-lg-end align-items-center gap-4">
+              <a href="mailto:info@intersection.test" class="text-decoration-none text-color-5 fw-bold">Contact us</a>
+              <a href="/privacy-policy.html" class="text-decoration-none text-color-5 fw-bold">Privacy Policy</a>
+            </div>
+          </div>
+          <div class="footer-bottom d-flex flex-column align-items-center gap-3">
+            <a href="index.php">
+              <img src="images/favi-1.png" style="width: 30px;">
+            </a>
+            <div>Copyright &copy; 2023. All rights reserved.</div>
+          </div>
+        </div>
+      </footer>
+      <?php endif; ?>
 
     </body>
   </html>
